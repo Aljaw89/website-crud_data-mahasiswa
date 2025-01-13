ae4b1df3095abee4f97853844fde034d70d3377b
Aljaw89 <wajaw1377@gmail.com>
2025-01-13 23:08:54 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,14 @@
+# Website CRUD Data Mahasiswa dengan Login Menggunakan Database dan PHP
+
+Aplikasi Website CRUD sederhana yang digunakan sebagai mendata Data-Data Mahasiswa/i UPB dengan dilengkapi fitur Register, Export File, Login dan Logout. Untuk perancangan aplikasi website ini memanfaatkan simple CSS, Database MySQL, dan PHP. Sedangkan untuk pemanggilan alamat website menggunakan server XAMPP.
+
+link download XAMPP : https://www.apachefriends.org/download.html
+
+# Akun Login
+Terdapat beberapa akun untuk login pada website data mahasiswa ini, seperti berikut :
+
+- Admin
+  Memiliki hak akses dalam menambah, menghapus, meng-export file serta melihat data mahasiswa UPB.
+  
+- Mahasiswa/i UNIV UPB
+  Memiliki hak akses dalam melihat data mahasiswa UPB sebagai bentuk cross-check kebenaran data.
---
+++ db/db_mahasiswa.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 13, 2025 at 02:06 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_mahasiswa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `username`, `password`) VALUES
+(0, 'Aljaw', 'e10adc3949ba59abbe56e057f20f883e');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `nim` varchar(20) NOT NULL,
+  `kelas` varchar(2) NOT NULL,
+  `jurusan` varchar(50) NOT NULL,
+  `semester` varchar(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`id`, `nama`, `nim`, `kelas`, `jurusan`, `semester`) VALUES
+(0, 'wahyu aldi r', '312310006', 'C1', 'Teknik Informatika', '3');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
